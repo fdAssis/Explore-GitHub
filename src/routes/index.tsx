28b89aa1d303bc816c973ab5033b3ec42c7abe51
fdAssis <fd07ass@gmail.com>
2020-09-17 22:20:28 -0300

    create routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+/**
+ * Pages
+ */
+import Dashboar from '../pages/Dashboard';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboar} />
+    <Route path="/repository" component={Repository} />
+  </Switch>
+);
+
+export default Routes;
