c3a3704994a4892d4622f5e991af18f51f3fdca3
fdAssis <fd07ass@gmail.com>
2020-09-17 23:02:37 -0300

    staled components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { Title } from './styles';
+import logoImg from '../../assets/logo_app.svg';
+
 const Dashboar: React.FC = () => {
-  return <h1> Dashboar </h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github logo" />
+      <Title> Explore repositorios no github </Title>
+    </>
+  );
 };
 
 export default Dashboar;
