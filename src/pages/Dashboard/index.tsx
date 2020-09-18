2fa760d30d183409b0cc840c5b85882bf5841821
fdAssis <fd07ass@gmail.com>
2020-09-17 23:35:36 -0300

    deshbord form

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
 import logoImg from '../../assets/logo_app.svg';
 
 const Dashboar: React.FC = () => {
@@ -8,6 +8,24 @@ const Dashboar: React.FC = () => {
     <>
       <img src={logoImg} alt="Github logo" />
       <Title> Explore repositorios no github </Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositorio" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/43320138?s=460&u=83e00cc7d76057e6eee42c085c444b47f16d87fa&v=4"
+            alt="Francisco"
+          />
+          <div>
+            <strong>francisco/repository</strong>
+            <p>Aqui tem uma descricoa</p>
+          </div>
+        </a>
+      </Repositories>
     </>
   );
 };
